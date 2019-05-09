b7d3706e054ea6a42d0ed5cd8fa7bbaf5aa31e0e
zhouLion <748583403@qq.com>
2019-05-09 11:02:00 +0800

    严控表单步骤

--- src/views/application/new.vue
+++ src/views/application/new.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="application-new">
-    <el-card>
+    <el-card shadow="hover">
       <div
-        slot="header"
-        class="layout row justify-space-between"
+        :style="{'backgroundColor': theme}"
+        class="layout pa-3 row justify-space-between"
       >
         <span>新建申请</span>
       </div>
@@ -17,13 +17,13 @@
           >
             <el-step title="填写基础信息" />
             <el-step title="填写休假请求" />
-            <el-step title="提交请求" />
+            <el-step title="准备提交" />
           </el-steps>
         </div>
       </el-card>
-      <el-card class="elevation-0">
+      <el-card class="elevation-0 p-relitive">
         <div
-          v-show="active == 0"
+          v-show="showAll == true || active == 0"
           class="row layout"
         >
           <el-form
@@ -32,6 +32,7 @@
             class="full-width"
             label-width="180px"
           >
+            <div class="subheading pa-3">一、填写基础信息</div>
             <el-form-item label="身份号">
               <el-input
                 v-model="form.id"
@@ -106,10 +107,16 @@
                 :style="{ width: '400px' }"
                 disabled
               >
+                <div slot="prepend">
+                  <el-button
+                    type="primary"
+                    @click="submitBaseInfo"
+                  >生成</el-button>
+                </div>
                 <el-button
                   v-if="formFinal.baseInfoId"
                   slot="append"
-                  :loading="onLoadingInfoId"
+                  :loading="onLoading"
                   class="success"
                   icon="el-icon-success"
                   type="success"
@@ -117,16 +124,16 @@
                 <el-button
                   v-else
                   slot="append"
-                  :loading="onLoadingInfoId"
+                  :loading="onLoading"
                   icon="el-icon-question"
                 />
               </el-input>
             </el-form-item>
-            <el-form-item>
-              <el-button
-                type="primary"
+            <el-form-item v-show="showAll == false">
+              <!-- <el-button
                 @click="submitBaseInfo"
-              >提交基础信息</el-button>
+                type="primary"
+              >提交基础信息</el-button>-->
               <el-button
                 :disabled="!formFinal.baseInfoId"
                 @click="active = 1"
@@ -135,7 +142,7 @@
           </el-form>
         </div>
         <div
-          v-show="active == 1"
+          v-show="showAll == true || active == 1"
           class="row layout"
         >
           <el-form
@@ -144,6 +151,7 @@
             class="full-width"
             label-width="180px"
           >
+            <div class="subheading pa-3">二、填写休假请求</div>
             <el-form-item label="申请理由">
               <el-input v-model="formApply.reason" />
             </el-form-item>
@@ -205,16 +213,42 @@
 
             <el-form-item label="休假目的地">
               <el-cascader
+                v-model="formApply.vocationPlaceArr"
                 :options="locationOptions"
                 @active-item-change="handleItemChange"
               />
             </el-form-item>
 
-            <el-form-item size="large">
-              <el-button
-                type="primary"
-                @click="submitRequestInfo"
-              >提交请求信息</el-button>
+            <el-form-item label="回执编号">
+              <el-input
+                v-model="formFinal.RequestId"
+                :style="{ width: '400px' }"
+                disabled
+              >
+                <div slot="prepend">
+                  <el-button
+                    type="primary"
+                    @click="submitRequestInfo"
+                  >生成</el-button>
+                </div>
+                <el-button
+                  v-if="formFinal.RequestId"
+                  slot="append"
+                  :loading="onLoading"
+                  class="success"
+                  icon="el-icon-success"
+                  type="success"
+                />
+                <el-button
+                  v-else
+                  slot="append"
+                  :loading="onLoading"
+                  icon="el-icon-question"
+                />
+              </el-input>
+            </el-form-item>
+
+            <el-form-item v-show="showAll == false">
               <el-button @click="active = 0">上一步</el-button>
               <el-button
                 :disabled="!formFinal.RequestId"
@@ -224,7 +258,7 @@
           </el-form>
         </div>
         <div
-          v-show="active == 2"
+          v-show="showAll == true || active == 2"
           class="row layout"
         >
           <el-form
@@ -233,6 +267,7 @@
             class="full-width"
             label-width="180px"
           >
+            <div class="subheading pa-3">最后一步、准备提交</div>
             <el-form-item label="基础信息回执编号">
               <el-input
                 :value="formFinal.baseInfoId"
@@ -245,16 +280,52 @@
                 disabled
               />
             </el-form-item>
-            <el-form-item>
-              <el-button @click="submitApply">提交</el-button>
+            <el-form-item v-if="formFinal.RequestId && formFinal.baseInfoId">
+              <el-alert
+                show-icon
+                title="信息填写完备，可以准备提交"
+                type="info"
+              />
+            </el-form-item>
+            <el-form-item v-show="showAll == false">
+              <el-button
+                type="primary"
+                @click="active = 3"
+              >准备提交</el-button>
               <el-button @click="active = 1">上一步</el-button>
             </el-form-item>
           </el-form>
         </div>
         <div
-          v-show="active == 3"
+          v-show="showAll == true || active == 3"
           class="row layout"
-        >asdfasdfsdfasdf</div>
+        />
+        <div
+          v-if="showAll"
+          class="mask"
+        />
+        <div
+          v-if="showAll"
+          :style="{'backgroundColor': theme}"
+          class="footer-nav"
+        >
+          <div
+            v-if="isAfterSubmit"
+            class="row layout justify-center fill-height"
+          >
+            <el-button @click="createNew">新建申请</el-button>
+          </div>
+          <div
+            v-else
+            class="row layout justify-center fill-height"
+          >
+            <el-button
+              v-loading="onLoading"
+              @click="submitApply"
+            >提交</el-button>
+            <el-button @click="active = 0">重新填写</el-button>
+          </div>
+        </div>
         <!-- card body -->
       </el-card>
     </el-card>
@@ -271,7 +342,7 @@ export default {
     return {
       active: 0,
       OnloadingUserInfoes: false,
-      onLoadingInfoId: false,
+      onLoading: false,
       form: {
         id: '',
         realName: '',
@@ -289,7 +360,8 @@ export default {
         VocationLength: 0,
         OnTripLength: 0,
         VocationType: '',
-        vocationPlace: '',
+        vocationPlace: 0,
+        vocationPlaceArr: [],
         reason: ''
       },
       formFinal: {
@@ -302,7 +374,16 @@ export default {
           value: 0,
           children: []
         }
-      ]
+      ],
+      isAfterSubmit: false
+    }
+  },
+  computed: {
+    showAll() {
+      return this.active === 3
+    },
+    theme() {
+      return this.$store.state.settings.theme
     }
   },
   methods: {
@@ -365,7 +446,7 @@ export default {
         Phone,
         Settle
       } = this.form
-      this.onLoadingInfoId = true
+      this.onLoading = true
       postBaseInfo({
         id,
         realName,
@@ -377,14 +458,14 @@ export default {
         Settle
       })
         .then(data => {
-          this.onLoadingInfoId = false
+          this.onLoading = false
           if (data.id) {
             this.formFinal.baseInfoId = data.id
             this.$message.success('成功提交，回执编号为：' + data.id)
           }
         })
         .catch(() => {
-          this.onLoadingInfoId = false
+          this.onLoading = false
           this.formFinal.baseInfoId = ''
           this.$message.error('基础信息提交失败，请检查填写信息')
         })
@@ -394,9 +475,17 @@ export default {
      * 提交请求信息
      */
     submitRequestInfo() {
-      const infoParam = Object.assign({}, this.formApply, {
+      if (this.onLoading === true) {
+        return this.$message.info('生成中，请等待')
+      }
+      const { vocationPlaceArr, ...param } = this.formApply
+      if (vocationPlaceArr && vocationPlaceArr.length > 0) {
+        param.vocationPlace = vocationPlaceArr[vocationPlaceArr.length - 1]
+      }
+      const infoParam = Object.assign({}, param, {
         id: this.form.id
       })
+      this.onLoading = true
       postRequestInfo(infoParam)
         .then(data => {
           const id = data.id
@@ -408,14 +497,21 @@ export default {
         .catch(() => {
           return this.$message.error('提交失败，请重新检查表单')
         })
+        .finally(() => {
+          this.onLoading = false
+        })
     },
 
     /**
      * 提交申请
      */
     submitApply() {
+      if (this.onLoading === true) {
+        return this.$message.info('提交中，请等待')
+      }
       const BaseId = this.formFinal.baseInfoId
       const RequestId = this.formFinal.RequestId
+      this.onLoading = true
       submitApply({
         RequestId,
         BaseId,
@@ -425,14 +521,49 @@ export default {
       })
         .then(data => {
           this.active = 3
+          this.isAfterSubmit = true
           return this.$message.success('保存成功')
         })
         .catch(() => {
           return this.$message.error('保存失败')
         })
+        .finally(() => {
+          return (this.onLoading = false)
+        })
     },
-    handleChange() {},
-    onSubmit() {}
+
+    /**
+     * 创建新的申请
+     */
+    createNew() {
+      this.active = 0
+      this.form = {
+        id: '',
+        realName: '',
+        company: '',
+        companyName: '',
+        duties: '',
+        HomeAddress: 0,
+        HomeDetailAddress: '',
+        Phone: 0,
+        Settle: 0
+      }
+      this.formApply = {
+        StampLeave: '2019-5-7',
+        VocationLength: 0,
+        OnTripLength: 0,
+        VocationType: '',
+        vocationPlace: 0,
+        vocationPlaceArr: [],
+        reason: ''
+      }
+      this.formFinal = {
+        baseInfoId: '',
+        RequestId: ''
+      }
+      this.onLoading = false
+      this.isAfterSubmit = false
+    }
   }
 }
 </script>
@@ -450,5 +581,31 @@ hr.divider {
 }
 .full-width {
   width: 100%;
+}
+.p-relitive {
+  position: relative;
+}
+.mask {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #e1f5fe47;
+  z-index: 1000;
+  cursor: not-allowed;
+}
+.footer-nav {
+  position: fixed;
+  bottom: 0;
+  min-height: 48px;
+  width: 100%;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  background: white;
+  box-shadow: 0 -2px 10px -4px;
+  border-radius: 4px 4px 0 0;
+  padding: 8px;
 }
 </style>
