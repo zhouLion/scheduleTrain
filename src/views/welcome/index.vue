ae2e02c39987b27a832106e91ca659d1db7b84ca
汉广 <liuchangm7j@vip.qq.com>
2019-05-12 14:53:08 +0800

    modify 显示icon

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -4,27 +4,30 @@
       <el-col :span="6">
         <router-link to="application/new">
           <el-card :body-style="{ padding: '0px' }">
-            <div style="padding: 14px;">
+            <div class="title">
               <span>创建申请</span>
             </div>
+            <svg-icon icon-class="测试申请" />
           </el-card>
         </router-link>
       </el-col>
       <el-col :span="6">
         <router-link to="login?redirect=application/examine">
           <el-card :body-style="{ padding: '0px' }">
-            <div style="padding: 14px;">
+            <div class="title">
               <span>审核申请</span>
             </div>
+            <svg-icon icon-class="提案审批" />
           </el-card>
         </router-link>
       </el-col>
       <el-col :span="6">
         <router-link to="login?redirect=application/list">
           <el-card :body-style="{ padding: '0px' }">
-            <div style="padding: 14px;">
+            <div class="title">
               <span>查询申请</span>
             </div>
+            <svg-icon icon-class="无序排列" />
           </el-card>
         </router-link>
       </el-col>
@@ -46,6 +49,22 @@ export default {
   width: 100%;
   height: 400px;
 }
+
+.title {
+  width: 10em;
+  padding-left: 1em;
+  padding-top: 1em;
+  font-stretch: 20px;
+}
+
+.svg-icon {
+  width: 15em;
+  height: 15em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+
 .welcome {
   height: 100vh;
   /* line-height: 100vh; */
