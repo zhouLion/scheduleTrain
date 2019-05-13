bd1067f8b0a3c8fc54d824980ff73feb2e2d1ffb
汉广 <liuchangm7j@vip.qq.com>
2019-05-13 10:05:38 +0800

    new 审核页面添加导出功能

--- src/views/application/examine.vue
+++ src/views/application/examine.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="application-examine ma-4">
     <el-row :gutter="20">
-      <el-col
-        :lg="4"
-        :md="4"
-        :sm="24"
-      >
-        <el-card
-          class="elevation-0"
-          shadow="hover"
-        >
-          <el-form
-            ref="queryForm"
-            :model="queryForm"
-          >
+      <el-col :lg="4" :md="4" :sm="24">
+        <el-card class="elevation-0" shadow="hover">
+          <el-form ref="queryForm" :model="queryForm">
             <el-row>
-              <el-col
-                :md="24"
-                :sm="12"
-                class="mb-3"
-              >
+              <el-col :md="24" :sm="12" class="mb-3">
                 <div class="py-2 body-2 layout align-center row justify-space-between">
                   单位
                   <el-button
@@ -36,10 +22,7 @@
                   placeholder="选择管辖单位"
                   @change="companyChanged"
                 >
-                  <el-option
-                    label="--全部--"
-                    value
-                  />
+                  <el-option label="--全部--" value />
                   <el-option
                     v-for="item in myManages"
                     :key="item.code"
@@ -50,11 +33,7 @@
 
                 <!-- <el-input v-model="queryForm.companyCode"></el-input> -->
               </el-col>
-              <el-col
-                :md="24"
-                :sm="12"
-                class="mb-3"
-              >
+              <el-col :md="24" :sm="12" class="mb-3">
                 <div class="py-2 body-2 layout align-center row justify-space-between">
                   指定人员
                   <el-switch v-model="queryForm.isSearchUser" />
@@ -65,10 +44,7 @@
                   class="full-width"
                   placeholder
                 >
-                  <el-option
-                    :value="$store.state.user.userid"
-                    label="查我自己"
-                  />
+                  <el-option :value="$store.state.user.userid" label="查我自己" />
                   <el-option
                     v-for="item in membersOption"
                     :key="item.id"
@@ -78,69 +54,52 @@
                 </el-select>
               </el-col>
 
-              <el-col
-                :span="24"
-                class="py-2"
-              >
-                <el-button
-                  class="full-width"
-                  type="primary"
-                  @click="searchData"
-                >查询</el-button>
+              <el-col :span="24" class="py-2">
+                <el-button class="full-width" type="primary" @click="searchData">查询</el-button>
               </el-col>
             </el-row>
           </el-form>
         </el-card>
       </el-col>
-      <el-col
-        :lg="20"
-        :md="20"
-        :sm="24"
-      >
+      <el-col :lg="20" :md="20" :sm="24">
         <!-- <div
           @click
           :class=""
           type="primary"
         >12</div>-->
 
-        <ApplicationList
-          :data-list="dataList"
-          :on-loading="onLoading"
-          @refresh="searchData"
-        >
-          <template
-            slot="action"
-            slot-scope="{row}"
-          >
+        <ApplicationList :data-list="dataList" :on-loading="onLoading" @refresh="searchData">
+          <template slot="headeraction">
+            <el-button icon="el-icon-edit" type="primary" @click="handleCreate">添加</el-button>
+
             <el-button
-              size="mini"
-              type="success"
-              @click="auditApply(row, 1)"
-            >通过</el-button>
+              v-if="queryForm.isSearchUser"
+              icon="el-icon-download"
+              type="primary"
+              @click="exportUserApplies({user: queryForm.userId})"
+            >导出用户申请</el-button>
+
+            <el-button
+              v-else
+              icon="el-icon-download"
+              type="primary"
+              @click="exportCompanyApplies({company: queryForm.companyCode})"
+            >导出单位申请</el-button>
+          </template>
+          <template slot="action" slot-scope="{row}">
+            <el-button size="mini" type="success" @click="auditApply(row, 1)">通过</el-button>
             <el-button
               v-if="row.status!='publish'"
               size="mini"
               type="warning"
               @click="auditApply(row, 2)"
             >驳回</el-button>
-            <el-button
-              v-if="row.status!='deleted'"
-              size="mini"
-              type="danger"
-            >删除</el-button>
+            <el-button v-if="row.status!='deleted'" size="mini" type="danger">删除</el-button>
           </template>
         </ApplicationList>
 
-        <el-dialog
-          :visible="auditForm.show"
-          title="提交审核"
-          width="30%"
-        >
-          <el-form
-            ref="auditForm"
-            :model="auditForm"
-            label-width="80px"
-          >
+        <el-dialog :visible="auditForm.show" title="提交审核" width="30%">
+          <el-form ref="auditForm" :model="auditForm" label-width="80px">
             <el-form-item label="审核结果">
               <el-switch
                 v-model="auditForm.action"
@@ -155,35 +114,19 @@
               />
             </el-form-item>
             <el-form-item label="备注">
-              <el-input
-                v-model="auditForm.remark"
-                placeholder="请输入备注"
-                type="textarea"
-              />
+              <el-input v-model="auditForm.remark" placeholder="请输入备注" type="textarea" />
             </el-form-item>
             <el-form-item label="安全码">
-              <el-input
-                v-model="auditForm.Code"
-                placeholder="请输入安全码"
-              />
+              <el-input v-model="auditForm.Code" placeholder="请输入安全码" />
             </el-form-item>
-            <el-form-item
-              hidden
-              label="审核人"
-            >
-              <el-input
-                v-model="auditForm.AuthByUserID"
-                placeholder="请输入审核人的id"
-              />
+            <el-form-item hidden label="审核人">
+              <el-input v-model="auditForm.AuthByUserID" placeholder="请输入审核人的id" />
             </el-form-item>
           </el-form>
 
           <span slot="footer">
             <el-button @click="auditForm.show = false">取 消</el-button>
-            <el-button
-              type="primary"
-              @click="SubmitAuditForm"
-            >确 定</el-button>
+            <el-button type="primary" @click="SubmitAuditForm">确 定</el-button>
           </span>
         </el-dialog>
       </el-col>
@@ -196,12 +139,38 @@ import ApplicationList from './components/ApplicationList'
 import { toCompany, toUser, audit } from '../../api/apply'
 import { getOnMyManage } from '../../api/usercompany'
 import { getMembers } from '../../api/company'
+import {
+  exportUserApplies,
+  exportApply,
+  exportCompanyApplies
+} from '../../api/static'
+// 将导出的方法以mixins的方式注入到vm实例
+const mixins = {
+  methods: {
+    exportUserApplies,
+    exportApply,
+    exportCompanyApplies,
+    download(data) {
+      if (!data) {
+        return
+      }
+      const url = window.URL.createObjectURL(new Blob([data]))
+      const link = document.createElement('a')
+      link.style.display = 'none'
+      link.href = url
+      link.setAttribute('download', 'excel.xlsx')
 
+      document.body.appendChild(link)
+      link.click()
+    }
+  }
+}
 export default {
   name: 'ApplyExamine',
   components: {
     ApplicationList
   },
+  mixins: [mixins],
   data() {
     return {
       queryForm: {
